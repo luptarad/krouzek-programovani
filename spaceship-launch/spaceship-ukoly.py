e88a4058f3f0605654b373f7528687be0dcbbf3a
luptarad <radovan.luptak@gmail.com>
2022-11-06 11:16:22 +0100

    spaceship hra
    in scratch:
    https://scratch.mit.edu/projects/651101717/

---
+++ spaceship-launch/spaceship-ukoly.py
@@ -0,0 +1,58 @@
+import random
+import sys
+
+print("Start kosmicke lodi")
+
+#NAPOVEDA - POTREBNE PRIKAZY
+#random.randint(1,10)   -   vrati nahodne cislo od 1 do 10
+#sys.exit(0)            -   prikaz pro ukonceni programu
+
+
+
+#NASTAVENI
+#Co vsechno je potreba nastavit?
+#Jake promenne budou potreba?
+...
+...
+...
+...
+
+
+#Vypis velikost gravitace
+print(???)
+
+#Vypis prazdny radek
+print(???)
+
+
+while i < 10:   # zacatek cyklusu
+    odpoved = int(input("Zadej silu motora:")) #otazka pro hrace
+
+    #Podminka: Prilis mala sila
+    if (odpoved < potrebna_sila_motoru):
+        print("Prilis malo")
+
+    #Dopln podminku: prilis velka sila
+    ...
+    ...
+    
+    #Dopln podminku: spravna odpoved
+    ...
+    ...
+    ...
+        
+    print("Zkus to znovu")
+    #Tady vypis dva prazdne radky
+    ...
+    ...
+    i = i + 1   # posledny prikaz cyklusu
+    
+
+#Vypis, ze se start nepovedl a co se s tebou stane:
+print (???)
+print (???)
+
+
+
+
+      
---
+++ spaceship-launch/spaceship.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+print("Start kosmicke lodi")
+
+#NASTAVENI
+gravitace = random.randint(1, 10)
+hmotnost_lode = random.randint(1, 20)
+potrebna_sila_motoru = gravitace * hmotnost_lode
+i = 0
+
+print("Gravitace =", gravitace)
+print("")
+
+while i < 10:
+    odpoved = int(input("Zadej silu motora:"))
+    if (odpoved < potrebna_sila_motoru):
+        print("Prilis malo")
+    if (odpoved > potrebna_sila_motoru):
+        print("Prilis moc")
+    if (odpoved == potrebna_sila_motoru):
+        print("Uspesny start! Jses zachranen!")
+        sys.exit(0)
+        
+    print("Zkus to znovu")
+    print("")
+    print("")
+    i = i + 1
+
+print("Start se ti nepovedl")
+print("Budes sezran vesmirnymi krabmi")
+
+
+
+
+
+
+
+      
